ab8cc03db876ea005a6d6355021026a75cbcaa4f
ShamindaKanchana <msk60310910@gmail.com>
2024-12-03 20:10:36 +0530

    content transfered to gui

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from transcription import validation,transcription
-from translation import translation
-user_input=input("Enter a DNA sequence:").upper()
+from gui import root 
 
-if validation(user_input):
-    print("This is a valid sequence")
-    rna_sequence=transcription(user_input)
-    print('RNA sequence is '+rna_sequence)
-    amino_acid_sequence=translation(rna_sequence)
-    print('Aminno  sequence is '+amino_acid_sequence)
-else:
-    print("This is invalid sequence")    
+if __name__=="__main__":
+    root.mainloop()
+ 
     
  
